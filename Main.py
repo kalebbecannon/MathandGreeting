12edb6833eedefc09ed6e4cc97e30e671e5c1f22
kalebbecannon <kalebbecannon@gmail.com>
2023-05-31 17:19:08 -0500

    first commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+print("Hello There")
+name = input("What is your name?")
+print("Hello there" + " " + name)
+
+
+value1 = int(input("Can you provide the first number?"))
+value2 = int(input("Now can you provide the second number?"))
+totalvalue = value1 * value2
+endMessage = ("Hello there again " + name + " the answer to your multiplication equation is: ")
+print(endMessage)
+print(totalvalue)
